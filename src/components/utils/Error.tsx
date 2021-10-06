2a8781870edaf50279e84504ee0284c000f3269e
Van Trong Hung 20173158 <hung.vt173158@sis.hust.edu.vn>
2021-10-06 18:22:04 +0700

    error notification response

--- src/components/utils/Error.tsx
+++ src/components/utils/Error.tsx
@@ -2,10 +2,10 @@ import { message } from "antd";
 export function errorAPI(error: any) {
     console.log("err___________");
     try {
-        for (const e of error.response.data.message) {
-            console.log("e", e);
-
-            message.error(e);
+        for (const err in error.response.data) {
+            for (const e of error.response.data[err]) {
+                message.error(e);
+            }
         }
     } catch {
         message.error("Đã có lỗi sảy ra, bạn vui lòng thử lại sau");
