46cd14cf8d0651960ea6c650ed3f322da4171089
Van Trong Hung 20173158 <hung.vt173158@sis.hust.edu.vn>
2021-10-06 18:17:31 +0700

    add active-user

---
+++ src/components/user/activate-user/index.tsx
@@ -0,0 +1,40 @@
+import { AxiosResponse } from "axios";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { ResponseActivateUser } from "../../../interface/request/UserRespository";
+import API from "../../../request";
+import { LOGIN } from "../../router/const";
+import { errorAPI } from "../../utils/Error";
+import { message } from "antd";
+
+const ActivateUser = () => {
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        async function activate() {
+            try {
+                const active_token: string =
+                    window.location.search.split("=")[1];
+                const res: AxiosResponse<ResponseActivateUser> =
+                    await API.user.activateUser({ active_token: active_token });
+                setLoading(false);
+                message.success(res.data.message[0]);
+            } catch (err: any) {
+                errorAPI(err);
+            }
+        }
+        activate();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...!</div>;
+    } else {
+        return (
+            <div>
+                Kích hoạt tài khoản thành công, bấm vào
+                <Link to={LOGIN}> đây</Link> để đăng nhập
+            </div>
+        );
+    }
+};
+export default ActivateUser;
