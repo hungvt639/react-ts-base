4390ea9e0b434a73bbea8e31b84b9046f4f7e274
Van Trong Hung 20173158 <hung.vt173158@sis.hust.edu.vn>
2021-10-06 18:15:31 +0700

    add active-user, reset-password. update router

--- src/components/router/const.ts
+++ src/components/router/const.ts
@@ -1,6 +1,8 @@
 export const LOGIN = "/login";
 export const REGISTER = "/register";
+export const SEND_RESET_PASSWORD = "/send_reset-password";
 export const RESET_PASSWORD = "/reset-password";
+export const ACTIVATE_USER = "/activate-user";
 export const HOME = "/";
 
 export const PROFILE = "/user/profile";
--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -1,17 +1,31 @@
 import React, { Suspense } from "react";
-import { LOGIN, REGISTER, HOME } from "./const";
+import {
+    LOGIN,
+    REGISTER,
+    HOME,
+    ACTIVATE_USER,
+    SEND_RESET_PASSWORD,
+    RESET_PASSWORD,
+} from "./const";
 import {
     Route,
     Redirect,
     Switch,
     BrowserRouter as Router,
 } from "react-router-dom";
+import { useSelector } from "react-redux";
 // import Home from '../home/content/home';
-
+import "./router.scss";
+import "./general.scss";
+import { AppState } from "../../interface/redux";
 const Login = React.lazy(() => import("../user/login"));
 const Register = React.lazy(() => import("../user/register"));
-const Screen = React.lazy(() => import("./Screen"));
-
+const PrivateRouter = React.lazy(() => import("./PrivateRouter"));
+const ActivateUser = React.lazy(() => import("../user/activate-user"));
+const SendResetPassword = React.lazy(
+    () => import("../user/send-reset-password")
+);
+const ResetPassword = React.lazy(() => import("../user/reset-password"));
 export function WaitingComponent(Component: any) {
     return (props: any) => (
         <Suspense fallback={<div>loading</div>}>
@@ -30,6 +44,21 @@ const Routers = () => {
                     path={REGISTER}
                     component={WaitingComponent(Register)}
                 />
+                <Route
+                    exact
+                    path={ACTIVATE_USER}
+                    component={WaitingComponent(ActivateUser)}
+                />
+                <Route
+                    exact
+                    path={SEND_RESET_PASSWORD}
+                    component={WaitingComponent(SendResetPassword)}
+                />
+                <Route
+                    exact
+                    path={RESET_PASSWORD}
+                    component={WaitingComponent(ResetPassword)}
+                />
                 <CheckLogin />
             </Switch>
         </Router>
@@ -37,9 +66,22 @@ const Routers = () => {
 };
 export default Routers;
 
-const CheckLogin = () => {
-    const token = localStorage.getItem("token");
+const CheckLogin = (props: any) => {
+    // const token = localStorage.getItem("token");
+    const token = useSelector((state: AppState) => state.userState.token);
+    console.log("token", token);
+
     if (token)
-        return <Route path={HOME} component={WaitingComponent(Screen)} />;
-    else return <Redirect to={LOGIN} />;
+        return (
+            <Route path={HOME} component={WaitingComponent(PrivateRouter)} />
+        );
+    else
+        return (
+            <Redirect
+                to={{
+                    pathname: LOGIN,
+                    search: `?next=${props.location.pathname}${props.location.search}`,
+                }}
+            />
+        );
 };
