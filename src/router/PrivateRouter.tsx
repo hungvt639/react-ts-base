fce060ee00263d580a1cd7fbcada3304a7ab119c
Van Trong Hung 20173158 <hung.vt173158@sis.hust.edu.vn>
2021-11-14 17:16:54 +0700

    Remove BrowserRouter

--- src/router/PrivateRouter.tsx
+++ src/router/PrivateRouter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../screens/home/header";
 import Slider from "../screens/home/slider";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { WaitingComponent } from ".";
 import { PROFILE, HOME } from "./const";
 
@@ -9,7 +9,7 @@ const Profile = React.lazy(() => import("../screens/home/content/profile"));
 const Home = React.lazy(() => import("../screens/home/content/home"));
 const PrivateRouter = () => {
     return (
-        <Router>
+        <>
             <Header />
             <div className="content">
                 <Slider />
@@ -22,7 +22,7 @@ const PrivateRouter = () => {
                     <Route path={HOME} component={WaitingComponent(Home)} />
                 </Switch>
             </div>
-        </Router>
+        </>
     );
 };
 export default PrivateRouter;
