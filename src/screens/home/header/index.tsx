439c4b76eb1a84d1300b3c20b99d9b18fe5bff7a
Van Trong Hung 20173158 <hung.vt173158@sis.hust.edu.vn>
2021-11-14 17:05:38 +0700

    btn language, profile, logout

--- src/screens/home/header/index.tsx
+++ src/screens/home/header/index.tsx
@@ -5,6 +5,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { clearUser } from "../../../store/actions/userAction";
 import { Dropdown } from "antd";
 import { AppState } from "../../../interface/redux";
+import { Link } from "react-router-dom";
+import { PROFILE } from "../../../router/const";
+import VIImg from "../../../img/vi.png";
+import ENImg from "../../../img/en.png";
+
 const Header = () => {
     const dispatch = useDispatch();
     const user = useSelector((state: AppState) => state.userState.user);
@@ -24,17 +29,30 @@ const Header = () => {
         localStorage.removeItem("token");
         // localStorage.removeItem("user");
     }
-    const menu = <div onClick={logout}>{t("Logout")}</div>;
+    const menu = (
+        <div>
+            <div>
+                <Link to={PROFILE}>Profile</Link>
+            </div>
+            <div onClick={logout}>{t("Logout")}</div>
+        </div>
+    );
     return (
         <div className="header">
-            <div className="max-width1200 headers margin-auto">
+            <div className="max-width1200 headers margin-0auto">
                 <div className="width-100 height-100">{t("Header")}</div>
                 <div className="align-items-center">
                     <div className="btn-header" onClick={onLangChange}>
+                        <img
+                            className="img-language"
+                            width="24px"
+                            alt="language"
+                            src={i18n.language === "vi" ? VIImg : ENImg}
+                        />
                         {t("L")}
                     </div>
                     <Dropdown overlay={menu} placement="bottomRight">
-                        <div className="btn-header">{`${user?.first_name} ${user?.last_name}`}</div>
+                        <div className="btn-header">{`${user?.last_name} ${user?.first_name}`}</div>
                     </Dropdown>
                 </div>
             </div>
