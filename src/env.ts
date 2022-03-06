d6573a60c581d50b11cded20fb71cc555096dcd6
Van Trong Hung 20173158 <hung.vt173158@sis.hust.edu.vn>
2022-03-06 15:51:17 +0700

    env for dev, local

--- src/env.ts
+++ src/env.ts
@@ -1,20 +1,7 @@
-// export const BASE_URL =
-//     process.env.REACT_APP_BASE_URL || "http://127.0.0.1:8000";
-// export const BASE_URL_IMG =
-//     process.env.REACT_APP_BASE_URL_IMG || "http://127.0.0.1:8000";
-
-export const BASE_URL =
-    process.env.REACT_APP_BASE_URL_HOME || "http://127.0.0.1:8000";
-export const BASE_URL_IMG =
-    process.env.REACT_APP_BASE_URL_IMG_HOME || "http://127.0.0.1:8000";
-
-// export const BASE_URL =
-//     process.env.REACT_APP_BASE_URL_BUILD || "http://127.0.0.1:8000";
-// export const BASE_URL_IMG =
-//     process.env.REACT_APP_BASE_URL_IMG_BUILD || "http://127.0.0.1:8000";
-
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const BASE_URL_IMG = process.env.REACT_APP_BASE_URL_IMG;
 export const KEY_GOOGLE_MAP = process.env.REACT_APP_KEY_GOOGLE_MAP;
-export const MQTT = process.env.REACT_APP_MQTT || "ws://20.212.16.22:9001";
+export const MQTT = process.env.REACT_APP_MQTT;
 export const IMGUR = process.env.REACT_APP_IMGUR;
 export const IMGUR_TOKEN = process.env.REACT_APP_IMGUR_TOKEN;
 
@@ -26,4 +13,5 @@ const env = {
     IMGUR,
     IMGUR_TOKEN,
 };
+
 export default env;
